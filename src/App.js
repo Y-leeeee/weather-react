a811c1645b90ab4c48f199f03b989cce2a47e741
Y-leeeee <lyr890802@gmail.com>
2023-09-17 21:54:01 +0200

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -23,38 +23,44 @@ export default function App() {
   };
   return (
     <div className="App">
-      <div className="weather-app">
-        <form className="change-city" onSubmit={handleSubmit}>
-          <input type="text" className="city-name" placeholder="Type a city" />
-          <input
-            type="submit"
-            className="btn btn-outline-dark"
-            id="search-button"
-            value="Search"
-          />
-          <input
-            type="submit"
-            className="btn btn-outline-dark"
-            id="current-city-btn"
-            value="Current"
-          />
-        </form>
+      <div className="container">
+        <div className="weather-app">
+          <form className="change-city" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="city-name"
+              placeholder="Type a city"
+            />
+            <input
+              type="submit"
+              className="btn btn-outline-dark"
+              id="search-button"
+              value="Search"
+            />
+            <input
+              type="submit"
+              className="btn btn-outline-dark"
+              id="current-city-btn"
+              value="Current"
+            />
+          </form>
 
-        <div className="city">{weatherData.city}</div>
-        <div className="current-time"></div>
-        <img src="#" alt="" className="icon-element" />
-        <div>
-          <span className="temperature-now">{weatherData.temperature}℃</span>
+          <div className="city">{weatherData.city}</div>
+          <div className="current-time"></div>
+          <img src="#" alt="" className="icon-element" />
+          <div>
+            <span className="temperature-now">{weatherData.temperature}℃</span>
+          </div>
+          <div className="description">{weatherData.description}</div>
+
+          <div className="details">
+            <div className="humidity">{weatherData.humidity}%</div>
+
+            <div className="wind">{weatherData.wind}km/h</div>
+          </div>
+
+          <div className="weather-forecast" id="forecast"></div>
         </div>
-        <div className="description">{weatherData.description}</div>
-
-        <div className="details">
-          <div className="humidity">{weatherData.humidity}%</div>
-
-          <div className="wind">{weatherData.wind}km/h</div>
-        </div>
-
-        <div className="weather-forecast" id="forecast"></div>
       </div>
     </div>
   );
