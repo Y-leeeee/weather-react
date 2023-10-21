fc3035f56fd54b858d337b94d6d6f99b2101837f
Y-leeeee <lyr890802@gmail.com>
2023-10-21 21:21:26 +0200

    Added API integraion

--- src/Weather.js
+++ src/Weather.js
@@ -2,48 +2,64 @@ import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 
-export default function Weather() {
-  const [temperature, setTemperature] = useState(null);
-  const apiKey = "bdb603847ff33c6odd47b612a380tf56";
-  let city = "Seoul";
-  const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  return (
-    <div className="container">
-      <div className="weather-app">
-        <form className="change-city">
-          <input
-            type="text"
-            className="city-name"
-            placeholder="Type a city"
-            autoFocus="on"
-          />
-          <input
-            type="submit"
-            className="btn btn-outline-dark"
-            id="search-button"
-            value="Search"
-          />
-          <input
-            type="submit"
-            className="btn btn-outline-dark"
-            id="current-city-button"
-            value="Current"
-          />
-        </form>
-        <h1 className="city">Jeju</h1>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.temperature,
+      wind: response.wind,
+      city: response.city,
+      humidity: response.humidity,
+      description: response.description,
+    });
+  }
 
-        <img
-          src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-          alt="weather icon"
-          className="icon-element "
-        />
-        <div className="description">Cloudy</div>
-        <div className="details">
-          <span className="degree">🌡️32℃ </span>
-          <span className="humidity">💧 65% </span>
-          <span className="wind"> 💨13m/s</span>
+  if (weatherData.ready) {
+    return (
+      <div className="container">
+        <div className="weather-app">
+          <form className="change-city">
+            <input
+              type="text"
+              className="city-name"
+              placeholder="Type a city"
+              autoFocus="on"
+            />
+            <input
+              type="submit"
+              className="btn btn-outline-dark"
+              id="search-button"
+              value="Search"
+            />
+            <input
+              type="submit"
+              className="btn btn-outline-dark"
+              id="current-city-button"
+              value="Current"
+            />
+          </form>
+          <h1 className="city">{setWeatherData.city}</h1>
+
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="weather icon"
+            className="icon-element "
+          />
+          <div className="description">{setWeatherData.description}</div>
+          <div className="details">
+            <span className="degree">🌡️{setWeatherData.temperature}℃ </span>
+            <span className="humidity">💧 {setWeatherData.humidity}% </span>
+            <span className="wind"> 💨{setWeatherData.wind}m/s</span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "bdb603847ff33c6odd47b612a380tf56";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
